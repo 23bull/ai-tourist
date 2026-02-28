3187ee48d51c363a7d4d4c2a925b353e134ed6de
”23bull” <mrlorenzino@gmail.com>
2026-02-28 14:19:47 +0200

    Add middleware protection

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,29 @@
-import createMiddleware from "next-intl/middleware";
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
-export default createMiddleware({
-  locales: ["en", "sv", "no", "da", "de", "el"],
-  defaultLocale: "en",
-  localePrefix: "always"
-});
+export function middleware(request: NextRequest) {
+  const auth = request.headers.get('authorization')
+
+  if (!auth) {
+    return new NextResponse('Auth required', {
+      status: 401,
+      headers: {
+        'WWW-Authenticate': 'Basic realm="Secure Area"',
+      },
+    })
+  }
+
+  const base64Credentials = auth.split(' ')[1]
+  const credentials = atob(base64Credentials)
+  const [username, password] = credentials.split(':')
+
+  if (username !== 'admin' || password !== 'HEMLIGT_LÖSEN') {
+    return new NextResponse('Unauthorized', { status: 401 })
+  }
+
+  return NextResponse.next()
+}
 
 export const config = {
-  matcher: ["/", "/(en|sv|no|da|de|el)/:path*"]
-};
+  matcher: '/:path*',
+}
